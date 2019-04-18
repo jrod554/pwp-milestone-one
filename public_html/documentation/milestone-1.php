978b9530b0797c7543fd379418449ccf6150308a
Joshua Rodriguez <jrodriguez332@cnm.edu>
2019-04-17 22:17:30 -0600

    Added files to pwp-milestone-one

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<a lang="en"></a>
+<head>
+	<meta charset="UTF-8">
+	<title>Milestone One</title>
+</head>
+<body>
+<div>
+	<p><b>Purpose:</b></p>
+	<br>
+	<p>Portfolio of my recent projects for professional companies.</p>
+</div>
+<div>
+	<p><b>Audience:</b></p>
+	<br>
+	<p>Tech companies or Video Game Studio.</p>
+</div>
+<div>
+	<p><b>Goal:</b></p>
+	<br>
+	<p>My goal is to present what I've worked on as a junior web developer and
+	eventually get hired gaining as much experience as I can so further down
+	the line I can work for a Developer Studio.</p>
+</div>
+<div>
+	<p><b>Persona:</b></p>
+	<br>
+	<ul>
+		<li><b>Name:</b> Geralt Riviera</li>
+		<li><b>Age:</b> 21</li>
+		<li><b>Job:</b> Junior Web Developer</li>
+		<p><b>Technology:</b></p>
+		<li>iphone8plus with iOS12</li>
+		<li>Desktop with Intel Core i5-8265U Processor, GTX 1070, and 16gb ram.</li>
+		<li>HP Laptop with Intel Core i5-8250U CPU @ 1.60GHz, Intel UHD Graphics 620.</li>
+<div>
+		<p><b>Attitude:</b></p>
+		<li>One of the many frustrations they deal with is self doubt. Even when they're in a good situation they believe
+		somehow it will end badly. It's natural with people but it ultimately depends on how the person deals with it.
+		Through dedication and hardwork, Geralt has his portfolio to show he didn't let stress get to him.</li>
+	<p><b>Goals:</b></p>
+		<li>Geralt's main goal is for a great tech company to look over his portfolio and eventually hire him. He wants
+	to spend as many years as he can to gain experience. Through years of working and making connections he hopes to one
+	day work for a Developer Studio. It would be a different experience, but important for him to learn. Further down the
+			line he hopes to one day own his own studio.</li>
+	</ul>
+</div>
+</body>
+</html>
